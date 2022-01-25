3c0a9f9ea0d2f87a2d82b3ef6eeb3555a6e885ab
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-01-25 18:34:33 -0500

    create the project

--- Weber/Weber/asgi.py
+++ Weber/Weber/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for hw1 project.
+ASGI config for Weber project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hw1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Weber.settings')
 
 application = get_asgi_application()
