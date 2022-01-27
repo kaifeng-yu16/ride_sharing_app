61ffd8e49b0193dd3ce84aa14b58ea4ca4bbe8bf
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-01-27 04:08:19 -0500

    add part of the login interface

--- Weber/user/urls.py
+++ Weber/user/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('login/', login.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', views.logout, name='logout'),
-    path('user_info/', views.change_info, name='change_info'), 
+    path('user_info/change', views.change_info, name='change_info'), 
+    path('create_account/', views.create_account, name='create_account'), 
 ]
--- Weber/user/views.py
+++ Weber/user/views.py
@@ -1,4 +1,8 @@
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -7,6 +11,22 @@ def welcome(request):
 
 def logout(request):
     return render(request, 'user/logout.html')
+
+def create_account(request):
+    if request.method == "GET":
+        return render(request, 'user/create_account.html')
+    else:
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        if User.objects.filter(username=username).exists():
+            return render(request, 'user/create_account.html', {'existed_user': True})
+        else: 
+            user = User.objects.create_user(username=username, password=password, email=email)
+            user.save()
+            messages.add_message(request, messages.INFO, 'Create Successfully! Please log in.')
+            return HttpResponseRedirect(reverse('user:login'))
+
 
 def change_info(request):
     return render(request, 'user/change_info.html')
