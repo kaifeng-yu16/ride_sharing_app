52d29d1b2a704f322da5a4c9f6f5e9c9fc761b03
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-01-26 20:42:11 -0500

    add welcome, logout page

--- Weber/user/urls.py
+++ Weber/user/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as login
+from . import views
 
+app_name = 'user'
 urlpatterns = [
+    path('', views.welcome, name='welcome'),
+    path('login/', login.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('user_info/', views.change_info, name='change_info'), 
 ]
--- Weber/user/views.py
+++ Weber/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def welcome(request):
+    return render(request, 'user/welcome.html')
+
+def logout(request):
+    return render(request, 'user/logout.html')
+
+def change_info(request):
+    return render(request, 'user/change_info.html')
