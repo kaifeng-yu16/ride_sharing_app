289fe0dc0b921276d762b62bcf52c738c7a8c7f3
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-01-27 23:14:22 -0500

    create_ride

--- Weber/ride/urls.py
+++ Weber/ride/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from Weber.ride import views
 
 urlpatterns = [
-    path('create_account/', views.create_ride, name='create_ride'),
+    path('create_ride/', views.create_ride, name='create_ride'),
 ]
