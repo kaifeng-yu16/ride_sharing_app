d0377d29136d7a83a054556320367f381e4d8b23
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-01-27 19:40:59 -0500

    add logout&home page

--- Weber/user/urls.py
+++ Weber/user/urls.py
@@ -6,7 +6,7 @@ app_name = 'user'
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('login/', login.LoginView.as_view(template_name='user/login.html'), name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/', login.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('user_info/change', views.change_info, name='change_info'), 
     path('create_account/', views.create_account, name='create_account'), 
 ]
