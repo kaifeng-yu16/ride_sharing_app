d1f18a2309f713f5d1d92291266970ac8a065864
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-01-28 21:54:45 -0500

    model

--- Weber/Weber/settings.py
+++ Weber/Weber/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'rideShareServiceDB',
-        'USER': 'kaifeng',
-        'PASSWORD': 'kaifeng',
+        'USER': 'fm128',
+        'PASSWORD': 'fm128',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
