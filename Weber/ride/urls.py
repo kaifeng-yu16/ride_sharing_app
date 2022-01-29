4c9b598d51c593d089852728c3766401e51010f7
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-01-29 01:50:24 -0500

    search_as_driver

--- Weber/ride/urls.py
+++ Weber/ride/urls.py
@@ -5,6 +5,6 @@ app_name = 'ride'
 urlpatterns = [
     path('create_ride/', views.create_ride, name='create_ride'),
     path('search_as_sharer/', views.search_as_sharer, name='search_as_sharer'),
-    path('search_as_rider/', views.search_as_rider, name='search_as_rider'),
+    path('search_as_driver/', views.search_as_driver, name='search_as_driver'),
     path('home/', views.home, name='home'),
 ]
