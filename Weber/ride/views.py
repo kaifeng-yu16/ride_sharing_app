08dda6fbc5923311e312978a3ace7d5713767f1a
Kaifeng Yu <kaifeng.yu625@outlook.com>
2022-01-31 17:14:55 -0500

    fixed a bug

--- Weber/ride/views.py
+++ Weber/ride/views.py
@@ -163,9 +163,10 @@ def sharer_join(request, ride_id):
         return HttpResponse('This ride was canceled by owner!')
     elif ride.status == 'open':
         if request.method == "POST":
-            if not ride.allow_share or ride.status != 'open' or (request.user.id, ) in ride.sharer_set.all().values_list('sharer') \
-                    or request.user == ride.owner:
+            if not ride.allow_share or ride.status != 'open' or request.user == ride.owner:
                 return HttpResponse('Invalid Access!')
+            if (request.user.id, ) in ride.sharer_set.all().values_list('sharer'):
+                return HttpResponse('You have already joined this ride!')
             num_of_sharers = request.POST['num_of_sharers']
             sharer = Sharer.objects.create(ride=ride, sharer=request.user, num_of_sharers=num_of_sharers)
             ride.num_passengers += int(num_of_sharers)
